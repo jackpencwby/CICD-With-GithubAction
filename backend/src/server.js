1dbd4da523dec44219650c96ff1b0b9f9ebc68f6
jackpencwby <kajonyod.jack@gmail.com>
2025-02-21 18:51:37 +0700

    config-database-and-use-.env

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT
 
 app.get("/", (req, res) => {
     res.send("Hello Express");
