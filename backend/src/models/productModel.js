64e99f5d9a09015153ca1d16d8ac5746f47770ed
jackpencwby <kajonyod.jack@gmail.com>
2025-02-24 16:00:47 +0700

    edit-attr-product-model

--- backend/src/models/productModel.js
+++ backend/src/models/productModel.js
@@ -3,11 +3,11 @@ import pool from "../config/db.js"
 const CreateProductTable = async () => {
     const query = `
         CREATE TABLE IF NOT EXISTS products (
-            Id SERIAL PRIMARY KEY,
-            ProductName VARCHAR(100),
-            Price DECIMAL(10, 2),
-            Stock INTEGER NOT NULL DEFAULT 0,
-            Description TEXT
+            id SERIAL PRIMARY KEY,
+            product_name VARCHAR(100),
+            price DECIMAL(10, 2),
+            stock INTEGER NOT NULL DEFAULT 0,
+            description TEXT
         );
     `;
 
